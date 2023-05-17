55a69f8912d08aa3aac8068de7f53e4a67c5f209
shweyee <shweyeemyintzutheinzan@gmail.com>
2023-05-17 15:32:11 +0630

    microservice

---
+++ api-security/src/main/java/com/example/apisecurity/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.example.apisecurity.controller;
+
+import com.example.apisecurity.data.User;
+import com.example.apisecurity.data.UserDao;
+import com.example.apisecurity.exception.PasswordNotMatchError;
+import com.example.apisecurity.service.AuthService;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+   private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    record RequestUser(
+            @JsonProperty("first_name") String firstName,
+            @JsonProperty("last_name")String lastName,
+            String email,
+            String password,
+            @JsonProperty("confirm_password") String confirmPassword){}
+
+    record RegisterResponse(
+        @JsonProperty("frist_name") String firstName,
+        @JsonProperty("last_name")String lastName,
+        String email){
+    }
+
+    @PostMapping("/register")
+    public RegisterResponse register(@RequestBody RequestUser requestUser){
+        if(!requestUser.password().equals(requestUser.confirmPassword())){
+            throw new PasswordNotMatchError();
+
+        }
+        User user=authService.register(requestUser.firstName(),
+                requestUser.lastName(),
+                requestUser.email(),
+                requestUser.password(),
+                requestUser.confirmPassword()
+        );
+        return new RegisterResponse(user.getFirstName(),user.getLastName(),user.getEmail());
+    }
+}
---
+++ api-security/src/main/java/com/example/apisecurity/data/User.java
@@ -0,0 +1,40 @@
+package com.example.apisecurity.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.annotation.PersistenceCreator;
+
+@ToString
+@Getter
+@Setter
+public class User {
+    @Id
+    private Long id;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private String email;
+    private String password;
+
+    public static User of(String firstName,String lastName,String email,String password) {
+        return new User(null,firstName,lastName,email,password);
+    }
+
+    public User() {
+    }
+
+    @PersistenceConstructor
+    public User(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
